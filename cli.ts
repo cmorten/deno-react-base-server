47c4b94d8357b0bc62fcb8a151cb9dcdc8c8b579
asos-craigmorten <craig.morten@asos.com>
2020-06-12 21:55:04 +0100

    feat: add missing path guard

--- cli.ts
+++ cli.ts
@@ -5,6 +5,11 @@ if (import.meta.main) {
   const { args } = Deno;
   const { path, port = 3000 } = parse(args);
 
+  if (!path) {
+    console.log("Missing '--path' flag. A valid URL / path to a module which exports a React component (which is the default export) must be provided.");
+    Deno.exit(1);
+  }
+
   baseServer({ appModulePath: path, port: port });
 
   console.log(`Listening on port ${port}`);
